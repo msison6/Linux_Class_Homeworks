e2c86f1284d27cd907392c33a6e39df28b1c5f0b
Marcus Sison <msison@njcu.edu>
2020-05-31 19:37:30 +0000

    This is why I am taking CS407 this summer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("My favorite programming language is Java");
+		System.out.println("I am taking CS407 because I want to learn and program Linux");
 	
 	}
+
 }
